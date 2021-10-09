65762fd16e25f6debf448a1562ed500d96ea3e26
chucuoi1 <khang140388@yahoo.com>
2021-10-09 21:24:00 +0700

    Update c.sh

--- c.sh
+++ c.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1OnqStbm36V6OyAvdoJ792aJjTS0_9r4_' -O C.zip
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1iO14ygq_d6tjpSsTdTZFd3irrmIdSZDR' -O C.zip
 unzip C.zip
