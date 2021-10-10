29c6ad6903fc3ff013fd5e46e0e0b153f9491aa0
chucuoi1 <khang140388@yahoo.com>
2021-10-10 13:27:09 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ sudo apt-get update
 wait
 sudo apt-get upgrade -y
 sudo apt install vnstat speedometer wget zip unzip curl git -y
-wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=16IECjxQe77yUF5mcU9uX0WTKxJjwSAR6' -O data.zip
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=17BeOs0PbnPVTJKz2YGODJzw397i4KObT' -O data.zip
 unzip data.zip
 #wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=16IECjxQe77yUF5mcU9uX0WTKxJjwSAR6' -O bash.zip
 #unzip bash.zip
