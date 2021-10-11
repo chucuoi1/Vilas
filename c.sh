6e1eb3ae777930675f18bbaa029d7adf3f93b083
chucuoi1 <khang140388@yahoo.com>
2021-10-11 15:17:16 +0700

    Update c.sh

--- c.sh
+++ c.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd ~/
-wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1iO14ygq_d6tjpSsTdTZFd3irrmIdSZDR' -O C.zip
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1iO14ygq_d6tjpSsTdTZFd3irrmIdSZDR' -O 'C.zip'
 unzip C.zip
