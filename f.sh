3ce7922b83b5f712db026abe3d46b8dc92c513be
chucuoi1 <khang140388@yahoo.com>
2021-10-11 15:15:39 +0700

    Update f.sh

--- f.sh
+++ f.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd ~/
-wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1RV2vklc6DOy02epkMLu9vPRSwKXA90CO' -O F.zip
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1RV2vklc6DOy02epkMLu9vPRSwKXA90CO' -O 'F.zip'
 unzip F.zip
