df24d7cfcec5f692aca7d89a2852244c51e3203b
chucuoi1 <khang140388@yahoo.com>
2021-10-11 15:12:56 +0700

    Update d.sh

--- d.sh
+++ d.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd ~/
-wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=185qOC4gGz__KknN10x5SL2JU683uLYss' -O D.zip
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=185qOC4gGz__KknN10x5SL2JU683uLYss' -O 'D.zip'
 unzip D.zip
