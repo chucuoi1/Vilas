948361a92425b57c789521251cc6a464d0b3207c
chucuoi1 <khang140388@yahoo.com>
2021-10-11 19:21:42 +0700

    Add files via upload

--- e.sh
+++ e.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cd '~/'
-wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1-tY0CpchQMwJagQApVn11GC4OYz2j863' -O 'E.zip'
+cd ~/
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1-tY0CpchQMwJagQApVn11GC4OYz2j863' -O E.zip
 unzip E.zip
