fd365e1736ee181bd9732e01470639bb3e719a35
chucuoi1 <khang140388@yahoo.com>
2021-10-11 19:35:11 +0700

    Update e.sh

--- e.sh
+++ e.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-cd /root/
-wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1-tY0CpchQMwJagQApVn11GC4OYz2j863' -O E.zip
-unzip E.zip
+cd ~/ && wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1-tY0CpchQMwJagQApVn11GC4OYz2j863' -O E.zip && unzip E.zip
