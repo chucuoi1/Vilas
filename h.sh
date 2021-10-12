79df71c1135da6967d8323aca6502c5ca82a139e
chucuoi1 <khang140388@yahoo.com>
2021-10-12 21:57:16 +0700

    Add files via upload

--- h.sh
+++ h.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cd ~/ && wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1QP_oYnbSJsakH7-4rExob3r6E7oPT1Hl' -O H.zip && unzip H.zip
+cd ~/ && wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=119YC-XVOhXOHXU662UDxa2qJnSJCrdG4' -O H.zip && unzip H.zip
