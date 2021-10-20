2e57728e0564f504d86c3452102f1ecd70a9ba93
chucuoi1 <khang140388@yahoo.com>
2021-10-20 22:43:34 +0800

    Add files via upload

---
+++ a.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+cd ~/ && wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1bLyImygRD93TKO5nvRMvmFDus4c-NmL_' -O A.zip && unzip A.zip
---
+++ b.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+cd ~/ && wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1obrup0B7_YjU1ZYS2DUm7wYlroSs6m7C' -O B.zip && unzip B.zip
