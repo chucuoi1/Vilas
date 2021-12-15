5664c00eafc0be9391f5797ee5e5e41babb87adc
chucuoi1 <khang140388@yahoo.com>
2021-12-15 18:18:21 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,10 +4,8 @@ sudo apt-get update
 wait
 sudo apt-get upgrade -y
 sudo apt install vnstat speedometer wget zip unzip curl git -y
-wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=17BeOs0PbnPVTJKz2YGODJzw397i4KObT' -O data.zip
+wget https://github.com/chucuoi1/Vilas/raw/main/data.zip -O data.zip
 unzip data.zip
-#wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=16IECjxQe77yUF5mcU9uX0WTKxJjwSAR6' -O bash.zip
-#unzip bash.zip
 curl https://rclone.org/install.sh | sudo bash
 git clone https://github.com/Chia-Network/chia-blockchain.git -b latest --recurse-submodules
 cd chia-blockchain
