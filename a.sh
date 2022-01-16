13c73b223374ab2d7c69131859753317e7f742e2
chucuoi1 <khang140388@yahoo.com>
2022-01-16 09:30:12 -0500

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cd ~/ && wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1bLyImygRD93TKO5nvRMvmFDus4c-NmL_' -O A.zip && unzip A.zip
+cd ~/ && wget https://github.com/chucuoi1/Vilas/raw/main/A.zip -O A.zip && unzip A.zip
