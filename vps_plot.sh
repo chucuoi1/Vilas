4a60f8533ff8458a43c0a58dca046bf2ec96af6d
chucuoi1 <khang140388@yahoo.com>
2022-03-30 22:36:53 +0800

    update

--- vps_plot.sh
+++ vps_plot.sh
@@ -21,6 +21,6 @@ sudo mount -t tmpfs -o rw,size=260G tmpfs /mnt/ramdisk2
 sleep 3
 bash /root/bash.sh
 sleep 3
-screen -dmS Plot1 ./chia-plotter/build/chia_plot -w -t /mnt/ramdisk1/ -d /mnt/Final/ -2 /mnt/ramdisk1/ -n -1 -r 90 -f 8c9db573edff069681e522055e35ded4f289f556de80dd8b96d2b6e403757faf6edb819b32c8ac9601df03c691ec4659 -c xch16ndcj6hew6a69tcn6h7dtrj744gd60mzp78hl70lr2047lnrnzdq7z8s5c
+bash /root/plot1.sh
 sleep 3
-screen -dmS Plot2 ./chia-plotter/build/chia_plot -w -t /mnt/ramdisk2/ -d /mnt/Final/ -2 /mnt/ramdisk2/ -n -1 -r 90 -f 8c9db573edff069681e522055e35ded4f289f556de80dd8b96d2b6e403757faf6edb819b32c8ac9601df03c691ec4659 -c xch16ndcj6hew6a69tcn6h7dtrj744gd60mzp78hl70lr2047lnrnzdq7z8s5c
+bash /root/plot2.sh
